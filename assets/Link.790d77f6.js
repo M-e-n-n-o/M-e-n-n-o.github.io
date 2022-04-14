52fe473dfd202fcb079cc83cb6c73cfd1e161d0e
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-04-14 23:15:00 +0200

    Updates

---
+++ assets/Link.790d77f6.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index.cb6d2041.js";const s=({children:e,link:r,newTab:a,className:t})=>o("a",{className:`px-2 py-1 bg-call hover:bg-call-hover text-white rounded-md ${t}`,href:r,target:a&&"_blank",children:e});export{s as L};
