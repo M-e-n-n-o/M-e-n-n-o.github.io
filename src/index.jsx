0c6c89c1e2cda3429ae129c1d4af07be820840fe
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-04-14 19:52:42 +0200

    [Change] Async component loading

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,27 @@
-import { createRoot } from "react-dom/client";
-import Front from "./views/Front";
-import ProjectsTimeline from "./views/ProjectsTimeline";
-import "./res/css/index.scss";
-import AboutMe from "./views/AboutMe";
-import Footer from "./views/Footer";
+import React, { Suspense } from "react";
 
+import { createRoot } from "react-dom/client";
+import "./res/css/index.scss";
+
+import Front from "./views/Front";
+import AboutMe from "./views/AboutMe";
 
 const container = document.getElementById("react-root");
 const root = createRoot(container);
+
+const ProjectsTimeline = React.lazy(() => import("./views/ProjectsTimeline"));
+const Footer = React.lazy(() => import("./views/Footer"));
 
 const Root = () => {
     return <>
         <Front />
         <AboutMe />
-        <ProjectsTimeline />
-        <Footer />
+        <Suspense fallback={<></>}>
+            <ProjectsTimeline />
+        </Suspense>
+        <Suspense fallback={<></>}>
+            <Footer />
+        </Suspense>
     </>;
 };
 
