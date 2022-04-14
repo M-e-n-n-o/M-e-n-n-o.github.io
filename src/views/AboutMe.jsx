65c5fba02b707a0af652b583ba5f37722186fa9b
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-04-14 21:37:29 +0200

    [Change] Beter sizing on large screens

--- src/views/AboutMe.jsx
+++ src/views/AboutMe.jsx
@@ -9,7 +9,7 @@ const AboutMe = () => {
                 <h2 className="text-[4rem] text-white">Over mij</h2>
             </Header>
             <div className="p-8 flex flex-col items-center justify-center sm:gap-12 sm:flex-row">
-                <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 2xl:w-1/8 flex items-center justify-center">
+                <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 2xl:w-1/2 flex items-center justify-center">
                     <img src={MennoImg} className="flex-1 aspect-1 rounded-md" />
                 </div>
                 <div className="text-[1.5rem] w-full flex-1 flex items-center py-4 sm:max-w-fit">
